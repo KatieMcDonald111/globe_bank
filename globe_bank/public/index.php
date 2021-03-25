02225b0ae3f46625020e8cef44dc6c3e873d90c2
KATIE MCDONALD <katielynnmcdonald@students.abtech.edu>
2021-03-25 15:27:10 -0400

    Final globe_bank files after completely chapter 6

---
+++ globe_bank/public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB 182</title>
+    <link href="globe_bank.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <h1>WEB 182</h1>
+    <p><a href="../../globe_bank/public/staff/">Globe Bank (staff)</a></p>
+
+  </body>
+</html>
