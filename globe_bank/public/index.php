3915d5d1d7be27457ad45fbc555a7c1dd6938864
KATIE MCDONALD <katielynnmcdonald@students.abtech.edu>
2021-04-12 15:11:56 -0400

    Starting Chapter 7

--- globe_bank/public/index.php
+++ globe_bank/public/index.php
@@ -3,15 +3,19 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WEB 182</title>
-    <link href="globe_bank.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="stylesheets/globe_bank.css" rel="stylesheet">
   </head>
 
   <body>
-
-    <h1>WEB 182</h1>
-    <p><a href="../../globe_bank/public/staff/">Globe Bank (staff)</a></p>
-
+    <header>
+      <img src="images/kitten.jpg" width="500" height="325" alt="orange kitten in sun">
+      <div id="headertext">
+        <h1>Ginger Kitten Design</h1>
+        <h2>-WEB 182-</h2>
+      </div>
+    </header>
+    <p><a href="../../globe_bank/public/staff/">Globe Bank: Staff</a></p>
   </body>
 </html>
